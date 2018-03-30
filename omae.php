68e93572e914ca2c3ebc51ab3a9ddc3732b90356
Victor Charbonnieras <victor.charbonnieras@isep.fr>
2018-03-30 11:43:41 +0200

    third commit

--- omae.php
+++ omae.php
@@ -41,7 +41,7 @@ This is a good meme
 
 <p>
 <?php
-echo "NEW MODIF v.2";
+echo "NEW MODIF";
 $feed = "<strong> Omae wa mou shindeiru ! </strong>";
 define("NANI", "<strong> NANI !? </strong>");
 //echo('Hello World!');
